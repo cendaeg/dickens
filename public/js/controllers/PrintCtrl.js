b0ad89d36e10c7b72c398aa537c7032d5b8f8f7c
Conlin Durbin <conlindurbin@gmail.com>
2015-12-20 11:26:43 -0500

    Change: Filter certificate by rural/city then last name

--- public/js/controllers/PrintCtrl.js
+++ public/js/controllers/PrintCtrl.js
@@ -116,44 +116,17 @@ app
     Settings.get().success(function(data, status, headers, config) {
         $scope.settings = data;
     });
-    $scope.dt1 = {};
-    $scope.dt2 = {};
-    $scope.today = function() {
-        $scope.dt1 = new Date();
-        $scope.dt2 = new Date();
-    };
-    $scope.today();
-
-    $scope.clear = function () {
-        $scope.dt1 = null;
-        $scope.dt2 = null;
-    };
-    $scope.open1 = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        $scope.opened1 = true;
-    };
-    $scope.open2 = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        $scope.opened2 = true;
-    };
-    $scope.dateOptions = {
-        formatYear: 'yy',
-        startingDay: 0
-    };
-    $scope.format = 'yyyy-MM-dd';
-    $scope.filterDate = function(item) {
-        return ((new Date(item.added).setHours(0,0,0,0)>=$scope.dt1.setHours(0,0,0,0)) && (new Date(item.added).setHours(0,0,0,0)<=$scope.dt2.setHours(0,0,0,0)));
-    }
-    $scope.validUntil = "January 31, 2016";
-    $scope.certDated = "Dec. 24, 2015";
+    var d = new Date();
+    var year = d.getFullYear();
+    $scope.validUntil = "January 31, "+(year+1);
+    $scope.certDated = "Dec. 24, "+year;
     $http.get('/api/family').success(function(data){
         var children = [];
         for(var i=0;i<data.length;i++) {
             for(var j=0;j<data[i].children.length;j++) {
+                console.log(data[i]);
                 data[i].children[j].added = data[i].added;
-		data[i].children[j].key = data[i].key;
+                data[i].children[j].key = data[i].key;
                 children.push(data[i].children[j]);
             }
         }
