949bbc68fd6894c1c7e644bade1d19bf4f364e87
Conlin Durbin <conlindurbin@gmail.com>
2015-12-20 13:17:49 -0500

    Fixing cert sorting

--- public/js/controllers/PrintCtrl.js
+++ public/js/controllers/PrintCtrl.js
@@ -121,8 +121,7 @@ app
   $scope.validUntil = "January 31, "+(year+1);
   $scope.certDated = "Dec. 24, "+year;
   $http.get('/api/family').success(function(data){
-    $scope.families = $filter('orderBy')(data, "['rural', 'l_name1']")
-    console.log(data, $scope.families);
+    $scope.families = $filter('orderBy')(data, ['rural', 'l_name1']);
   });
   $scope.getConsecutiveIndex = function(parentIndex, $index) {
     var total = 1;
